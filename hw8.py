6c65f3239f8f4bdf30f78e08767fa16bfcf0b403
Anastasiya <anastasiya.kovpak@gmail.com>
2023-08-12 09:44:06 +0300

    HW8_update

--- hw8.py
+++ hw8.py
@@ -43,6 +43,8 @@ else:
 5 10 15 20 25 30
 6 12 18 24 30 36'''
 
+#blabla bla
+
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows+1):
         for j in range(1, num_columns+1):
